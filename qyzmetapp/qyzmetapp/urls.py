99f5c2a645cec5f9fc1c91c5730a3676197364dc
yerke_aorus <zat.tyme.su@gmail.com>
2025-04-06 22:36:50 +0500

    simple jwt auth

--- qyzmetapp/qyzmetapp/urls.py
+++ qyzmetapp/qyzmetapp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
